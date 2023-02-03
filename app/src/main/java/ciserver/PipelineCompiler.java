857b69ff3681490a65d093d5555604f3c648dfcb
Didrik Munther <dmu0817@gmail.com>
2023-02-03 21:33:28 +0100

    Add calling gradle from java

---
+++ app/src/main/java/ciserver/PipelineCompiler.java
@@ -0,0 +1,45 @@
+package ciserver;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+
+public class PipelineCompiler implements PipelineComponent {
+	/**
+	 * Pulls the repository and checks out the head_commit
+	 * 
+	 * Inspiration from:
+	 * https://github.com/centic9/jgit-cookbook/blob/master/src/main/java/org/dstadler/jgit/unfinished/PullRemoteRepository.java
+	 * Can only pull from public repositories.
+	 * 
+	 * @return the status of the pull action
+	 */
+	public PipelineStatus execute(String pipelineDir, PushEvent event) {
+		var path = String.format("%s/repositories/%s", pipelineDir, event.headCommit.id);
+		var directory = new File(path);
+		directory.mkdirs(); // Make the directories recursively
+
+		try {
+			var process = new ProcessBuilder("sh", "gradlew", "build")
+					.directory(directory)
+					.redirectErrorStream(true)
+					.start();
+
+			// Here we have access to the output of the build, which we can send to GitHub.
+			String output = IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
+			System.err.println(output);
+
+			process.waitFor();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return PipelineStatus.Fail;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return PipelineStatus.Fail;
+		}
+
+		return PipelineStatus.Ok;
+	}
+}

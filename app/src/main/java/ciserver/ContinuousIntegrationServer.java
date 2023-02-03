soffan-group-25/ci-server

commit a3a75de26d2af46cc6e454478e402a590c119c73

feat: Add PushEvent datastructure (#27)
Fixes: #26 

- Adds a `PushEvent` class

app/src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -12,17 +12,20 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
-class TestObject {
-    public Integer thing;
-    public String other;
-}
-
-/**
- * Skeleton of a ContinuousIntegrationServer which acts as webhook
- * See the Jetty documentation for API documentation of those classes.
- */
 public class ContinuousIntegrationServer extends AbstractHandler {
+    Gson gson = new Gson();
+
+    private void handlePushEvent(PushEvent event) {
+        System.err.printf("%s, %s", event.ref, event.headCommit.url);
+
+        // Here you do all the continuous integration tasks
+        // For example:
+        // 1st clone your repository
+        // 2nd compile the code
+    }
+
     public void handle(String target,
             Request baseRequest,
             HttpServletRequest request,
@@ -32,27 +35,24 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
+        var eventType = request.getHeader("X-GitHub-Event");
         var body = request.getReader().lines().collect(Collectors.joining());
-        System.out.println(target);
 
-        // Print JSON body (as a string)
-        // System.out.printf("%s\n", body);
-
-        // Show that json parsing is possible
-        Gson gson = new Gson();
-        var obj = gson.fromJson(body, TestObject.class);
-        System.out.printf("%d\n", obj.thing);
-        System.out.printf("%s\n", obj.other);
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
+        try {
+            switch (eventType) {
+                case "push":
+                    handlePushEvent(gson.fromJson(body, PushEvent.class));
+                    break;
+                default: // Unimplemented, simply ignore
+            }
+        } catch (JsonSyntaxException e) {
+            System.err.printf("There was an error parsing the JSON for event of type %s\n", eventType);
+        }
 
         response.getWriter().println("CI job done");
     }
 
-    // used to start the CI server in command line
+    // Used to start the CI server in command line
     public static void startServer() throws Exception {
         Server server = new Server(8080);
         server.setHandler(new ContinuousIntegrationServer());

app/src/main/java/ciserver/PushEvent.java
@@ -1,0 +1,39 @@
+package ciserver;
+
+import java.util.ArrayList;
+
+import com.google.gson.annotations.SerializedName;
+
+class Repository {
+	String name;
+	String full_name;
+}
+
+class Author {
+	String name;
+	String email;
+	String username;
+}
+
+class Commit {
+	String id;
+	String url;
+
+	Author author;
+
+	ArrayList<String> added; // added files
+	ArrayList<String> removed; // removed files
+	ArrayList<String> modified; // modified files
+}
+
+public class PushEvent {
+	String ref; // hold branch name
+	String compare; // link to see changes
+
+	Author pusher;
+	Repository repository;
+	ArrayList<Commit> commits;
+
+	@SerializedName("head_commit")
+	Commit headCommit;
+}

app/src/test/java/ciserver/PushEventTest.java
@@ -1,0 +1,22 @@
+package ciserver;
+
+import org.junit.Test;
+import com.google.gson.Gson;
+import static org.junit.Assert.*;
+
+public class PushEventTest {
+    Gson gson = new Gson();
+
+    @Test
+    public void parsesPushEvent() {
+        // Typical push event
+        var data = "{	\"ref\": \"refs/heads/main\",	\"before\": \"e52bb2c1c1e7f09422d7eacf022afa61d7dce0da\",	\"after\": \"2c8db6c6d079f23710da7b1a61d9cb04fb366e04\",	\"repository\": {		\"id\": 596590165,		\"node_id\": \"R_kgDOI48-VQ\",		\"name\": \"ci-test\",		\"full_name\": \"didrikmunther/ci-test\",		\"private\": true,		\"owner\": {			\"name\": \"didrikmunther\",			\"email\": \"5240046+didrikmunther@users.noreply.github.com\",			\"login\": \"didrikmunther\",			\"id\": 5240046,			\"node_id\": \"MDQ6VXNlcjUyNDAwNDY=\",			\"avatar_url\": \"https://avatars.githubusercontent.com/u/5240046?v=4\",			\"gravatar_id\": \"\",			\"url\": \"https://api.github.com/users/didrikmunther\",			\"html_url\": \"https://github.com/didrikmunther\",			\"followers_url\": \"https://api.github.com/users/didrikmunther/followers\",			\"following_url\": \"https://api.github.com/users/didrikmunther/following{/other_user}\",			\"gists_url\": \"https://api.github.com/users/didrikmunther/gists{/gist_id}\",			\"starred_url\": \"https://api.github.com/users/didrikmunther/starred{/owner}{/repo}\",			\"subscriptions_url\": \"https://api.github.com/users/didrikmunther/subscriptions\",			\"organizations_url\": \"https://api.github.com/users/didrikmunther/orgs\",			\"repos_url\": \"https://api.github.com/users/didrikmunther/repos\",			\"events_url\": \"https://api.github.com/users/didrikmunther/events{/privacy}\",			\"received_events_url\": \"https://api.github.com/users/didrikmunther/received_events\",			\"type\": \"User\",			\"site_admin\": false		},		\"html_url\": \"https://github.com/didrikmunther/ci-test\",		\"description\": null,		\"fork\": false,		\"url\": \"https://github.com/didrikmunther/ci-test\",		\"forks_url\": \"https://api.github.com/repos/didrikmunther/ci-test/forks\",		\"keys_url\": \"https://api.github.com/repos/didrikmunther/ci-test/keys{/key_id}\",		\"collaborators_url\": \"https://api.github.com/repos/didrikmunther/ci-test/collaborators{/collaborator}\",		\"teams_url\": \"https://api.github.com/repos/didrikmunther/ci-test/teams\",		\"hooks_url\": \"https://api.github.com/repos/didrikmunther/ci-test/hooks\",		\"issue_events_url\": \"https://api.github.com/repos/didrikmunther/ci-test/issues/events{/number}\",		\"events_url\": \"https://api.github.com/repos/didrikmunther/ci-test/events\",		\"assignees_url\": \"https://api.github.com/repos/didrikmunther/ci-test/assignees{/user}\",		\"branches_url\": \"https://api.github.com/repos/didrikmunther/ci-test/branches{/branch}\",		\"tags_url\": \"https://api.github.com/repos/didrikmunther/ci-test/tags\",		\"blobs_url\": \"https://api.github.com/repos/didrikmunther/ci-test/git/blobs{/sha}\",		\"git_tags_url\": \"https://api.github.com/repos/didrikmunther/ci-test/git/tags{/sha}\",		\"git_refs_url\": \"https://api.github.com/repos/didrikmunther/ci-test/git/refs{/sha}\",		\"trees_url\": \"https://api.github.com/repos/didrikmunther/ci-test/git/trees{/sha}\",		\"statuses_url\": \"https://api.github.com/repos/didrikmunther/ci-test/statuses/{sha}\",		\"languages_url\": \"https://api.github.com/repos/didrikmunther/ci-test/languages\",		\"stargazers_url\": \"https://api.github.com/repos/didrikmunther/ci-test/stargazers\",		\"contributors_url\": \"https://api.github.com/repos/didrikmunther/ci-test/contributors\",		\"subscribers_url\": \"https://api.github.com/repos/didrikmunther/ci-test/subscribers\",		\"subscription_url\": \"https://api.github.com/repos/didrikmunther/ci-test/subscription\",		\"commits_url\": \"https://api.github.com/repos/didrikmunther/ci-test/commits{/sha}\",		\"git_commits_url\": \"https://api.github.com/repos/didrikmunther/ci-test/git/commits{/sha}\",		\"comments_url\": \"https://api.github.com/repos/didrikmunther/ci-test/comments{/number}\",		\"issue_comment_url\": \"https://api.github.com/repos/didrikmunther/ci-test/issues/comments{/number}\",		\"contents_url\": \"https://api.github.com/repos/didrikmunther/ci-test/contents/{+path}\",		\"compare_url\": \"https://api.github.com/repos/didrikmunther/ci-test/compare/{base}...{head}\",		\"merges_url\": \"https://api.github.com/repos/didrikmunther/ci-test/merges\",		\"archive_url\": \"https://api.github.com/repos/didrikmunther/ci-test/{archive_format}{/ref}\",		\"downloads_url\": \"https://api.github.com/repos/didrikmunther/ci-test/downloads\",		\"issues_url\": \"https://api.github.com/repos/didrikmunther/ci-test/issues{/number}\",		\"pulls_url\": \"https://api.github.com/repos/didrikmunther/ci-test/pulls{/number}\",		\"milestones_url\": \"https://api.github.com/repos/didrikmunther/ci-test/milestones{/number}\",		\"notifications_url\": \"https://api.github.com/repos/didrikmunther/ci-test/notifications{?since,all,participating}\",		\"labels_url\": \"https://api.github.com/repos/didrikmunther/ci-test/labels{/name}\",		\"releases_url\": \"https://api.github.com/repos/didrikmunther/ci-test/releases{/id}\",		\"deployments_url\": \"https://api.github.com/repos/didrikmunther/ci-test/deployments\",		\"created_at\": 1675347426,		\"updated_at\": \"2023-02-02T14:17:06Z\",		\"pushed_at\": 1675428495,		\"git_url\": \"git://github.com/didrikmunther/ci-test.git\",		\"ssh_url\": \"git@github.com:didrikmunther/ci-test.git\",		\"clone_url\": \"https://github.com/didrikmunther/ci-test.git\",		\"svn_url\": \"https://github.com/didrikmunther/ci-test\",		\"homepage\": null,		\"size\": 0,		\"stargazers_count\": 0,		\"watchers_count\": 0,		\"language\": null,		\"has_issues\": true,		\"has_projects\": true,		\"has_downloads\": true,		\"has_wiki\": true,		\"has_pages\": false,		\"has_discussions\": false,		\"forks_count\": 0,		\"mirror_url\": null,		\"archived\": false,		\"disabled\": false,		\"open_issues_count\": 2,		\"license\": null,		\"allow_forking\": true,		\"is_template\": false,		\"web_commit_signoff_required\": false,		\"topics\": [],		\"visibility\": \"private\",		\"forks\": 0,		\"open_issues\": 2,		\"watchers\": 0,		\"default_branch\": \"main\",		\"stargazers\": 0,		\"master_branch\": \"main\"	},	\"pusher\": {		\"name\": \"didrikmunther\",		\"email\": \"5240046+didrikmunther@users.noreply.github.com\"	},	\"sender\": {		\"login\": \"didrikmunther\",		\"id\": 5240046,		\"node_id\": \"MDQ6VXNlcjUyNDAwNDY=\",		\"avatar_url\": \"https://avatars.githubusercontent.com/u/5240046?v=4\",		\"gravatar_id\": \"\",		\"url\": \"https://api.github.com/users/didrikmunther\",		\"html_url\": \"https://github.com/didrikmunther\",		\"followers_url\": \"https://api.github.com/users/didrikmunther/followers\",		\"following_url\": \"https://api.github.com/users/didrikmunther/following{/other_user}\",		\"gists_url\": \"https://api.github.com/users/didrikmunther/gists{/gist_id}\",		\"starred_url\": \"https://api.github.com/users/didrikmunther/starred{/owner}{/repo}\",		\"subscriptions_url\": \"https://api.github.com/users/didrikmunther/subscriptions\",		\"organizations_url\": \"https://api.github.com/users/didrikmunther/orgs\",		\"repos_url\": \"https://api.github.com/users/didrikmunther/repos\",		\"events_url\": \"https://api.github.com/users/didrikmunther/events{/privacy}\",		\"received_events_url\": \"https://api.github.com/users/didrikmunther/received_events\",		\"type\": \"User\",		\"site_admin\": false	},	\"created\": false,	\"deleted\": false,	\"forced\": false,	\"base_ref\": null,	\"compare\": \"https://github.com/didrikmunther/ci-test/compare/e52bb2c1c1e7...2c8db6c6d079\",	\"commits\": [{		\"id\": \"2c8db6c6d079f23710da7b1a61d9cb04fb366e04\",		\"tree_id\": \"5dab785edbd133e97e3b93384fccd7de903d51db\",		\"distinct\": true,		\"message\": \"bbb\",		\"timestamp\": \"2023-02-03T13:48:12+01:00\",		\"url\": \"https://github.com/didrikmunther/ci-test/commit/2c8db6c6d079f23710da7b1a61d9cb04fb366e04\",		\"author\": {			\"name\": \"Didrik Munther\",			\"email\": \"dmu0817@gmail.com\",			\"username\": \"didrikmunther\"		},		\"committer\": {			\"name\": \"Didrik Munther\",			\"email\": \"dmu0817@gmail.com\",			\"username\": \"didrikmunther\"		},		\"added\": [],		\"removed\": [],		\"modified\": [\"README.md\"]	}],	\"head_commit\": {		\"id\": \"2c8db6c6d079f23710da7b1a61d9cb04fb366e04\",		\"tree_id\": \"5dab785edbd133e97e3b93384fccd7de903d51db\",		\"distinct\": true,		\"message\": \"bbb\",		\"timestamp\": \"2023-02-03T13:48:12+01:00\",		\"url\": \"https://github.com/didrikmunther/ci-test/commit/2c8db6c6d079f23710da7b1a61d9cb04fb366e04\",		\"author\": {			\"name\": \"Didrik Munther\",			\"email\": \"dmu0817@gmail.com\",			\"username\": \"didrikmunther\"		},		\"committer\": {			\"name\": \"Didrik Munther\",			\"email\": \"dmu0817@gmail.com\",			\"username\": \"didrikmunther\"		},		\"added\": [],		\"removed\": [],		\"modified\": [\"README.md\"]	}}";
+        var e = gson.fromJson(data, PushEvent.class);
+
+        var head = e.headCommit;
+        assertEquals(head.id, "2c8db6c6d079f23710da7b1a61d9cb04fb366e04");
+        assertEquals(e.commits.size(), 1);
+        assertEquals(e.pusher.name, "didrikmunther");
+        assertEquals(e.ref, "refs/heads/main");
+    }
+}

commit acf5b2b029a66b5cf4759ee688fbb40d0156a8eb

fix: Make `eventType` an Optional (#34)
## Describe your changes

Fixes #31. A null pointer exception was introduced in
a3a75de26d2af46cc6e454478e402a590c119c73 when the request didn't contain
the `X-GitHub-Event` header.

## Checklist before requesting review

- [x] Feature/fix PRs should add one atomic (as small as possible)
feature or
        fix
- [x] If your PR adds a feature or fix it also needs to add or modify at
least
        one test
-   [x] All code in your PR needs to have been formatted
- [x] The merge commit title should follow our commit prefix convention
of
        either "feat", "fix", "doc", or "refactor"
-   [x] The merge commit body should reference at least one issue
-   [x] The code compiles and all the tests pass

---------

Co-authored-by: Didrik Munther <dmu0817@gmail.com>

app/src/main/java/ciserver/ContinuousIntegrationServer.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.eclipse.jetty.server.Server;
@@ -15,6 +16,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
 public class ContinuousIntegrationServer extends AbstractHandler {
+
     Gson gson = new Gson();
 
     private void handlePushEvent(PushEvent event) {
@@ -35,18 +37,26 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
 
-        var eventType = request.getHeader("X-GitHub-Event");
+        var eventType = Optional.ofNullable(request.getHeader("X-GitHub-Event"));
         var body = request.getReader().lines().collect(Collectors.joining());
 
         try {
-            switch (eventType) {
+            if (!eventType.isPresent()) {
+                throw new Exception("'X-GitHub-Event' header is not present.");
+            }
+
+            switch (eventType.get()) {
                 case "push":
+                    System.out.println(body);
                     handlePushEvent(gson.fromJson(body, PushEvent.class));
                     break;
                 default: // Unimplemented, simply ignore
             }
+
         } catch (JsonSyntaxException e) {
-            System.err.printf("There was an error parsing the JSON for event of type %s\n", eventType);
+            System.err.printf("There was an error parsing the JSON for event of type %s\n", eventType.get());
+        } catch (Exception e) {
+            System.err.println(e);
         }
 
         response.getWriter().println("CI job done");
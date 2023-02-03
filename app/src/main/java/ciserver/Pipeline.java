e79d7255b21d67572345f19560bfc4be8cfef532
Didrik Munther <dmu0817@gmail.com>
2023-02-03 15:05:02 +0100

    Add pipeline class

---
+++ app/src/main/java/ciserver/Pipeline.java
@@ -0,0 +1,103 @@
+package ciserver;
+
+import java.util.ArrayList;
+
+// Steal from https://github.com/soffan-group-25/ci-server/pull/27
+class Commit {
+	String id;
+	String url;
+
+	ArrayList<String> added; // added files
+	ArrayList<String> removed; // removed files
+	ArrayList<String> modified; // modified files
+}
+
+enum PipelineStatus {
+	OK,
+	NotStarted,
+	InProgress
+}
+
+/**
+ * A PipelineInstance is responsible for the whole CI-pipeline
+ * for a specific commit.
+ */
+class PipelineInstance {
+	Commit commit;
+	Pipeline pipeline; // Parent pipeline
+	PipelineStatus status;
+
+	PipelineInstance(Commit commit, Pipeline pipeline) {
+		this.commit = commit;
+		this.pipeline = pipeline;
+		this.status = PipelineStatus.NotStarted;
+	}
+
+	private PipelineStatus _start() {
+		// The following pipeline process could
+		// be refactored into something more ergonomic and elegant.
+		// This will do for now.
+
+		var status = pull();
+		if (status != PipelineStatus.OK) {
+			return status;
+		}
+
+		status = lint();
+		if (status != PipelineStatus.OK) {
+			return status;
+		}
+
+		status = compile();
+		if (status != PipelineStatus.OK) {
+			return status;
+		}
+
+		return PipelineStatus.OK;
+	}
+
+	/**
+	 * Start the pipeline.
+	 * 
+	 * @return the status of the executed pipeline. OK if everything went ok.
+	 */
+	public PipelineStatus start() {
+		this.status = PipelineStatus.InProgress;
+		this.status = _start();
+
+		return this.status;
+	}
+
+	private PipelineStatus pull() {
+		return PipelineStatus.OK;
+	}
+
+	private PipelineStatus lint() {
+		return PipelineStatus.OK;
+	}
+
+	private PipelineStatus compile() {
+		return PipelineStatus.OK;
+	}
+}
+
+/**
+ * Main Pipeline object which creates PipelineInstances.
+ */
+public class Pipeline {
+	String pipelineFile; // Which directory to store pipeline data
+
+	Pipeline(String pipelineFile) {
+		this.pipelineFile = pipelineFile;
+	}
+
+	/**
+	 * Create new PipelineInstance
+	 * 
+	 * @param commit the commit to be worked on
+	 * @return the pipeline instance
+	 */
+	public PipelineInstance create(Commit commit) {
+		return new PipelineInstance(commit, this);
+	}
+}

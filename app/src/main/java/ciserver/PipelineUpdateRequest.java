cdf5f69eb764c309c566263764633330297ffddb
Daniel Williams <dwilli@kth.se>
2023-02-04 16:23:39 +0100

    Add the ability to send HTTP requests

--- app/src/main/java/ciserver/PipelineUpdateRequest.java
+++ app/src/main/java/ciserver/PipelineUpdateRequest.java
@@ -1,6 +1,12 @@
 package ciserver;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Optional;
+import com.google.gson.Gson;
 
 enum CommitStatus {
     ERROR,
@@ -45,15 +51,40 @@ class RequestBody {
 }
 
 public class PipelineUpdateRequest {
+    static final String API_VERSION = "2022-11-28";
+
     RequestPath path;
     RequestBody body;
     Optional<PipelineStage> failedOn;
+    String auth_tkn;
 
-    PipelineUpdateRequest(String owner, String repo, String sha, CommitStatus state, String url, String desc,
-            String ctx, PipelineStage failedOn) {
+    PipelineUpdateRequest(String owner, String repo, String sha, String token, CommitStatus state, String url,
+            String desc, String ctx, PipelineStage failedOn) {
         this.path = new RequestPath(owner, repo, sha);
         String state_str = state.toString().toLowerCase();
         this.body = new RequestBody(state_str, url, desc, ctx);
         this.failedOn = Optional.ofNullable(failedOn);
+        this.auth_tkn = token;
+    }
+
+    public int send() throws InterruptedException, IOException {
+        Gson gson = new Gson();
+        String request_body = gson.toJson(this.body);
+
+        // Build the actual request
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.github.com/repos/" + this.path.owner + "/" + this.path.repository
+                        + "/statuses/" + this.path.sha))
+                .POST(HttpRequest.BodyPublishers.ofString(request_body))
+                .header("Accept", "application/vnd.github+json")
+                .header("Authorization", "Bearer: " + this.auth_tkn)
+                .header("X-GitHub-Api-Version", API_VERSION)
+                .build();
+
+        // May throw InterruptedException or IOException.
+        // Note that it does NOT throw on bad status codes, only internal errors.
+        HttpResponse<String> resp = client.send(req, HttpResponse.BodyHandlers.ofString());
+        return resp.statusCode();
     }
 }

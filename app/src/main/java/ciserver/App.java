4867e40ae06967eeef580d000c3a872442c17649
haval0 <56519858+haval0@users.noreply.github.com>
2023-02-07 19:42:35 +0100

    Document the App.java class with JavaDoc

--- app/src/main/java/ciserver/App.java
+++ app/src/main/java/ciserver/App.java
@@ -3,11 +3,24 @@
  */
 package ciserver;
 
+/**
+ * This class is the initial class used to start the whole CI server program
+ */
 public class App {
+    /**
+     * Gets a greeting, in this case "Hello world!"
+     *
+     * @return String of the greeting to print
+     */
     public String getGreeting() {
         return "Hello World!";
     }
 
+    /**
+     * Print a greeting then launch the CI server
+     *
+     * @param args the arguments to the program, not used
+     */
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
 

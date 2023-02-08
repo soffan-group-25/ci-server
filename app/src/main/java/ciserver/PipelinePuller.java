835aa54714282b9370c2100cb5ec9c0afe178d49
haval0 <56519858+haval0@users.noreply.github.com>
2023-02-08 19:08:21 +0100

    doc: add javadoc to PipelinePuller.java (#60)
    resolves #59 
    
    All public classes must have JavaDoc. formatting makes this commit
    bigger than it should be

--- app/src/main/java/ciserver/PipelinePuller.java
+++ app/src/main/java/ciserver/PipelinePuller.java
@@ -7,43 +7,51 @@ import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
+/**
+ * This class is used early in our CI pipeline to pull the relevant ref from
+ * GitHub
+ */
 public class PipelinePuller implements StageTask {
 
-	/**
-	 * Pulls the repository and checks out the head_commit
-	 *
-	 * Inspiration from:
-	 * https://github.com/centic9/jgit-cookbook/blob/master/src/main/java/org/dstadler/jgit/unfinished/PullRemoteRepository.java
-	 * Can only pull from public repositories.
-	 *
-	 * @return the status of the pull action
-	 */
-	public PipelineStatus execute(String pipelineDir, PushEvent event) {
-		// Use the head_commit id as name for the repository directory
-		String directoryPath = String.format("%s/%s/%s", pipelineDir, event.repository.name, event.headCommit.id);
+    /**
+     * Pulls the repository and checks out the head_commit
+     *
+     * Inspiration from:
+     * https://github.com/centic9/jgit-cookbook/blob/master/src/main/java/org/dstadler/jgit/unfinished/PullRemoteRepository.java
+     * Can only pull from public repositories.
+     *
+     * @param pipelineDir the (grand)parent directory to run the commands in
+     * @param event       specifies a subdirectory based on the repo name and head
+     *                    commit id
+     *
+     * @return the status of the pull action
+     */
+    public PipelineStatus execute(String pipelineDir, PushEvent event) {
+        // Use the head_commit id as name for the repository directory
+        String directoryPath = String.format("%s/%s/%s", pipelineDir, event.repository.name, event.headCommit.id);
 
-		try {
-			File directory = new File(directoryPath);
-			directory.mkdirs(); // Make the directories recursively
-			FileUtils.deleteDirectory(directory); // If there already is a repository, delete it
+        try {
+            File directory = new File(directoryPath);
+            directory.mkdirs(); // Make the directories recursively
+            FileUtils.deleteDirectory(directory); // If there already is a repository, delete it
 
-			Git.cloneRepository()
-					.setURI(event.repository.cloneUrl)
-					.setDirectory(directory)
-					.setBranch(event.ref)
-					.call() // Clone the repository
-					.checkout()
-					.setName(event.headCommit.id)
-					.call(); // Checkout the commit
+            Git.cloneRepository()
+                    .setURI(event.repository.cloneUrl)
+                    .setDirectory(directory)
+                    .setBranch(event.ref)
+                    .call() // Clone the repository
+                    .checkout()
+                    .setName(event.headCommit.id)
+                    .call(); // Checkout the commit
 
-		} catch (GitAPIException e) {
-			e.printStackTrace();
-			return PipelineStatus.Fail;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return PipelineStatus.Fail;
-		}
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+            return PipelineStatus.Fail;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return PipelineStatus.Fail;
+        }
 
-		return PipelineStatus.Ok;
-	}
+        return PipelineStatus.Ok;
+    }
 }

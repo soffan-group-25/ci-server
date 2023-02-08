77f1f5eeb5ba19b4c4eb03601f440ef844fc3630
haval0 <56519858+haval0@users.noreply.github.com>
2023-02-08 15:15:50 +0100

    format PushEvent.java

--- app/src/main/java/ciserver/PushEvent.java
+++ app/src/main/java/ciserver/PushEvent.java
@@ -5,41 +5,41 @@ import java.util.ArrayList;
 import com.google.gson.annotations.SerializedName;
 
 class Repository {
-	String name;
-	String full_name;
+    String name;
+    String full_name;
 
-	@SerializedName("clone_url")
-	String cloneUrl;
+    @SerializedName("clone_url")
+    String cloneUrl;
 }
 
 class Author {
-	String name;
-	String email;
-	String username;
+    String name;
+    String email;
+    String username;
 }
 
 class Commit {
-	String id;
-	String url;
+    String id;
+    String url;
 
-	Author author;
+    Author author;
 
-	ArrayList<String> added; // added files
-	ArrayList<String> removed; // removed files
-	ArrayList<String> modified; // modified files
+    ArrayList<String> added; // added files
+    ArrayList<String> removed; // removed files
+    ArrayList<String> modified; // modified files
 }
 
 /**
  * This class represents the relevant information from a Push Event to GitHub
  */
 public class PushEvent {
-	String ref; // hold branch name
-	String compare; // link to see changes
+    String ref; // hold branch name
+    String compare; // link to see changes
 
-	Author pusher;
-	Repository repository;
-	ArrayList<Commit> commits;
+    Author pusher;
+    Repository repository;
+    ArrayList<Commit> commits;
 
-	@SerializedName("head_commit")
-	Commit headCommit;
+    @SerializedName("head_commit")
+    Commit headCommit;
 }
